0b2c32fa12f0c4261ed4a0ce3079d06021b6047d
angelo-squadra <angelo.savioti@squadra.com.br>
2021-05-10 17:57:04 -0300

    convertendo json

--- lib/app/shared/services/services_users.dart
+++ lib/app/shared/services/services_users.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:estudo_bloc/app/shared/models/user.dart';
 import 'package:estudo_bloc/app/shared/repositories/repository_users.dart';
@@ -7,10 +9,13 @@ class UsersService {
 
   Future<List<User>> getUsers() async {
     try {
-      List<User> users = [];
       Response _result = await _usersRepository.getUsersData();
       if (_result.statusCode != 200) throw _result.statusMessage;
-      print(_result.data);
+      final List<User> users = [];
+      List<dynamic> data = jsonDecode(_result.data);
+      data.forEach((i) {
+        print(i);
+      });
       return users;
     } catch (e) {
       throw e;
